a032c8d0734f8d612d489c7f6cba914351250bbe
Dmitrii Priimak <priimak69@inbox.ru>
2023-06-23 19:17:53 +0700

    Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,14 @@
 ﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-Console.WriteLine("Hello, World!");
+int Sum(int gmt)
+{
+    int res=0;
+    while (gmt > 0)
+    {
+        res = res + gmt % 10;
+        gmt = gmt / 10;
+    }
+    return res;
+}
+Console.WriteLine("Input namber: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Sum(namber));
