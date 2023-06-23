36ce3c36063b3987294012f4076bb04a0a63876d
Dmitrii Priimak <priimak69@inbox.ru>
2023-06-23 12:57:01 +0700

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,3 +1,16 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) 
 //и возводит число A в натуральную степень B.
-Console.WriteLine("Hello, World!");
+int Degree (int A; int B)
+{
+    int i = 0;
+    int result = A;
+    while (i <= B)
+    {
+        result = result + A;
+        i++;
+    }
+    return result;
+}
+int res = Degree();
+Console.WriteLine("A to the power B is", res);
+Console.WriteLine("", Degree());
