fdca7e483402b91b6cd432bf26a9135d9e702c23
Dmitrii Priimak <priimak69@inbox.ru>
2023-06-23 21:07:34 +0700

    Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,21 @@
 ﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-Console.WriteLine("Hello, World!");
+int [] Arrey (int size_arrey)
+{
+    int [] arrey1 = new int [size_arrey];
+    for (int i = 0; i < size_arrey; i++)
+    {
+        arrey1[i] = new Random().Next(0, 99);
+    }
+    return arrey1;
+}
+
+void PrintArrey (int [] arrey_print)
+{
+    Console.WriteLine("Arrey: ");
+    for (int i = 0; i < arrey_print.Length; i++)
+        Console.Write (arrey_print[i] + " ");
+}
+
+int size = 8;
+int [] arrey2 = Arrey(size);
+PrintArrey (arrey2);
